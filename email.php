8645faebdb712e25085da0f4643c9d98f16b0c74
Vitormnz <vitorhugomnz@gmail.com>
2022-07-20 22:36:13 -0300

    Projeto que fiz para uma empresa local ainda to aprendendo a usar git, relevem os erros pf :)

---
+++ email.php
@@ -0,0 +1,27 @@
+<html>
+    <body>
+        <?php
+            if(isset($_POST['Nome']) && !empty($_POST['Nome'])){
+                    $nome = addslashes($_POST['Nome']);
+                    $email = addslashes($_POST['Email']);
+                    $telefone = addslashes($_POST['Telefone']);
+
+                    $to = "vitorhugomnz@gmail.com";
+                    //gessonacionalsp@hotmail.com
+                    $subuject = "Contato para Orçamento Gesso nacional";
+
+                    $corpoDoEmail = "Nome" + $nome + "\r\n" + "Email" + $email + "\r\n" + "Telefone" + $telefone;
+
+                    $header = "From:contato@gessonacionalsp.com" + "\r\n" + "Reply-to:" + $email + "\r\n" + "X=mailer:PHP" + phpversion();
+
+             if(mail($to, $subuject, $corpoDoEmail ,$header)){
+                    echo ("<h3>Email enviado com sucesso</h3>" + "\r\n");
+                }else{
+                     echo ("Email não enviado, tente novamente")
+        
+                    <a href="index.html">Voltar</a>
+                    }
+            }
+        ?>
+    </body>
+</html>
