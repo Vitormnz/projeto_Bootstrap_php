488920cb4c8cb767b4c678f070a08973ebe46a6f
Vitor H. M. Bichara <82483199+Vitormnz@users.noreply.github.com>
2022-07-20 23:14:23 -0300

    arrumei o erro 500

--- email.php
+++ email.php
@@ -1,27 +1,20 @@
-<html>
-    <body>
-        <?php
+<?php
             if(isset($_POST['Nome']) && !empty($_POST['Nome'])){
                     $nome = addslashes($_POST['Nome']);
                     $email = addslashes($_POST['Email']);
                     $telefone = addslashes($_POST['Telefone']);
 
-                    $to = "test@gmail.com";
-                    //gessonacionalsp@hotmail.com
+                    $to = "gessonacionalsp@hotmail.com";
                     $subuject = "Contato para Orçamento Gesso nacional";
 
-                    $corpoDoEmail = "Nome" + $nome + "\r\n" + "Email" + $email + "\r\n" + "Telefone" + $telefone;
+                    $corpoDoEmail = "Nome" . $nome . "\r\n" . "Email" . $email . "\r\n" . "Telefone" . $telefone;
 
-                    $header = "From:contato@gessonacionalsp.com" + "\r\n" + "Reply-to:" + $email + "\r\n" + "X=mailer:PHP" + phpversion();
+                    $header = "From:contato@gessonacionalsp.com" . "\r\n" . "Reply-to:". $email . "\r\n" . "X=mailer:PHP" . phpversion();
 
              if(mail($to, $subuject, $corpoDoEmail ,$header)){
-                    echo ("<h3>Email enviado com sucesso</h3>" + "\r\n");
+                    echo ("<h3>Email enviado com sucesso</h3>" . "\r\n");
                 }else{
-                     echo ("Email não enviado, tente novamente")
-        
-                    <a href="index.html">Voltar</a>
-                    }
+                     echo ("Email não enviado, tente novamente");
+                    
+                }
             }
-        ?>
-    </body>
-</html>
